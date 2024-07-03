3995eab06a5ae8ce8bb59d0599b525f5b578b53b
Vivek Kumar Sahu <vivekkumarsahu650@gmail.com>
2024-07-03 14:06:49 +0530

    able to retrieve file data from url path
    Signed-off-by: Vivek Kumar Sahu <vivekkumarsahu650@gmail.com>

--- main.go
+++ main.go
@@ -2,10 +2,10 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
 	"net/url"
 	"os"
-	"path/filepath"
 	"strings"
 
 	"github.com/go-git/go-billy/v5/memfs"
@@ -42,6 +42,9 @@ func main() {
 			repoURL := gitURL.String()
 			fmt.Println("repoURL: ", repoURL)
 
+			filePath := strings.Join([]string{pathElems[len(pathElems)-2], pathElems[len(pathElems)-1]}, "/")
+			fmt.Println("filePath: ", filePath)
+
 			cloneOptions := &git.CloneOptions{
 				URL:           repoURL,
 				ReferenceName: plumbing.ReferenceName(fmt.Sprintf("refs/heads/%s", "main")),
@@ -57,16 +60,34 @@ func main() {
 			fmt.Println("repo: ", repo)
 
 			gitPathToJson := "/"
-			path := filepath.Clean(gitPathToJson)
-			fmt.Println("Path: ", path)
-			if _, err := fs.Stat(path); err != nil {
-				log.Fatalf("Failed to retrieve file Info: %s", err)
-			}
 
-			files, err := fs.ReadDir(path)
-			fmt.Println("files: ", files)
+			allFiles, err := source.ListFiles(fs, gitPathToJson, source.IsJson)
 			if err != nil {
-				log.Fatalf("Failed to read path: %s", err)
+				log.Fatalf("error: failed to list all files in repository (%w)", err)
+				fmt.Println("error: failed to list all files in repository (%w)", err)
+			}
+			fmt.Println("all Files: ", allFiles)
+			var ourFiles []string
+
+			for _, file := range allFiles {
+				if strings.Contains(file, filePath) {
+					fmt.Println("Yes, this file is ours")
+					ourFiles = append(ourFiles, file)
+				}
+			}
+			fmt.Println("ourFiles: ", ourFiles)
+
+			for _, file := range ourFiles {
+				filePath, err := fs.Open(file)
+				if err != nil {
+					fmt.Println("error: failed to open file(%w)", err)
+				}
+				data, err := io.ReadAll(filePath)
+				if err != nil {
+					fmt.Println("error: failed to read file(%w)", err)
+				}
+				fmt.Println("data: ", string(data))
+
 			}
 
 		}
--- source/source.go
+++ source/source.go
@@ -1,7 +1,55 @@
 package source
 
-import "regexp"
+import (
+	"fmt"
+	"io/fs"
+	"log"
+	"path/filepath"
+	"regexp"
+
+	"github.com/go-git/go-billy/v5"
+)
 
 func IsGit(in string) bool {
 	return regexp.MustCompile("^(http|https)://").MatchString(in)
+}
+
+func IsJson(file fs.FileInfo) bool {
+	if file.IsDir() {
+		return false
+	}
+	ext := filepath.Ext(file.Name())
+	return ext == ".json"
+}
+
+func ListFiles(fs billy.Filesystem, path string, predicate func(fs.FileInfo) bool) ([]string, error) {
+	path = filepath.Clean(path)
+	if _, err := fs.Stat(path); err != nil {
+		log.Fatalf("Failed to retrieve file Info: %s", err)
+	}
+	fmt.Println("Path: ", path)
+
+	files, err := fs.ReadDir(path)
+	if err != nil {
+		log.Fatalf("Failed to read path: %s", err)
+	}
+	fmt.Println("files: ", files)
+
+	var results []string
+
+	for _, file := range files {
+		name := filepath.Join(path, file.Name())
+		fmt.Println("name: ", name)
+
+		if file.IsDir() {
+			children, err := ListFiles(fs, name, predicate)
+			if err != nil {
+				return nil, err
+			}
+			results = append(results, children...)
+		} else if predicate(file) {
+			results = append(results, name)
+		}
+	}
+	return results, nil
 }
